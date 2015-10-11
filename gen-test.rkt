cf171d90bf863da56ac4f55ad20919cc50e730a9
Aleksandr Bezzubikov <abezzubikov@ispras.ru>
2015-10-11 19:47:43 +0300

    DFS works, bruteforce works too. Many graphs generation remaining

--- gen-test.rkt
+++ gen-test.rkt
@@ -8,36 +8,88 @@
       (if (= 0 res) (gen-length num) res)))
   
   (define (gen-graph num)
+    (define (gen-edge)
+      (let ((in (random num))(out (random num)))
+        (if (= in out) (gen-edge) (cons in out))))
     (define (add-edge cur)
-      (if (> 0 cur) '()
-          (cons (cons (random num)(random num)) (add-edge (- cur 1)))))
+      (if (< cur 0) '()
+          (cons (gen-edge) (add-edge (- cur 1)))))
     (remove-duplicates (add-edge (+ (/ num 2) (random num)))))
   
   (define (count-verts graph)
-    (foldl (lambda (a res)(max (car a) (cdr a) res)) graph))
+    (foldl (lambda (a res)(max (car a) (cdr a) res)) 0 graph))
   
-  (define (start-dfs graph cycles vis end)
+  (define (cyclic? lst1 lst2)
+    (let* ((tail (member (car lst1) lst2))(pos (- (length lst2) (if (equal? #f tail) 0 (length tail)))))
+      (equal?
+       lst1
+       (append (drop lst2 pos) (take lst2 pos))
+       )))
+  
+  (define (dfs graph)
     
-    (define (dfs edge graph cur-cycle cycles)
-      (begin (printf "Vert ~v\n" (car edge))
-      (if (and (not (member (car edge) end)) (member (car edge) vis))
-          (cons (cons (car edge) cur-cycle) cycles)
-          (let ((vis (cons (car edge) vis)))
-           (for-each
-            (lambda (arg)(dfs arg graph (cons (car edge) cur-cycle) cycles))
-            (filter (lambda (arg) (= (cadr edge)(car arg))) graph)
-            )         
-           (cons (car edge) end)
-           ))))
+    (define (dfs-step edge vis cur-cycle)
+      (begin
+        (if (member (cdr edge) vis)
+            (list (cons (car edge) (member (cdr edge)(reverse cur-cycle))))
+            (foldl
+             (lambda (arg result) (append (dfs-step arg (cons (car edge) vis) (cons (car edge) cur-cycle)) result))
+             '()
+             (filter (lambda (arg) (= (cdr edge) (car arg))) graph)
+             )
+            )))
     
-    (for-each (lambda (arg) (dfs arg graph '() cycles)) graph)
-    cycles)
+    (define (start-dfs edge)
+      (begin
+        (let ((cycle (dfs-step edge '() '())))
+          cycle 
+          )))
+    (begin
+      (printf "Graph: ~v\n" graph)
+      
+      (remove-duplicates (foldl
+                          (lambda (vert result)
+                            (begin
+                              (append (foldl
+                                       (lambda (edge result1) (append (start-dfs edge) result1))
+                                       '()
+                                       (filter (lambda (edge) (= (car edge) vert)) graph)) result)
+                              
+                              ))
+                          '()
+                          (range (add1 (count-verts graph)))) cyclic?)
+      ))
   
   
-  (define (find-cycles)
-    (start-dfs (list (list 0 1) (list 1 2) (list 2 3) (list 3 0)) '() '() '()))
+  (define (comb lst k)
+    (cond
+      ((zero? k) '(()))
+      ((null? lst) '())
+      (else (append
+             (map (lambda (x) (cons (car lst) x)) (comb (cdr lst) (sub1 k)))
+             (comb (cdr lst) k)))))
   
-  (find-cycles)
+  (define (bruteforce graph cycles depth)
+    (if (< K depth)
+        #f
+        (let ((result (ormap(lambda(verts)
+                              (andmap(lambda(cycle)
+                                       (ormap (lambda(cycle-vert)(member cycle-vert verts))
+                                              cycle))
+                                     cycles))
+                            (comb (range (add1 (count-verts graph))) depth))))
+          (if (equal? result
+                      #f
+                      )
+              (bruteforce graph cycles (add1 depth))
+              result))))
+  
+  (define (find-cycles graph)
+    (dfs graph));(gen-graph(read))))
+  
+  (define (start-bruteforce graph)
+    (bruteforce graph (find-cycles graph) 1))
+  (start-bruteforce  (list (cons 0 1) (cons 1 2) (cons 2 0) (cons 3 4) (cons 4 5) (cons 5 3)))
   )
 
-(gen-test 0)
+(gen-test 2)
